8c5d02a5048c2a3d09fc514619faa03c35697f89
nek_vasia <nek_vasia@mail.ru>
2023-12-16 14:19:02 +0300

    Добавил пункт phone

--- src/class/ProductSearch.php
+++ src/class/ProductSearch.php
@@ -4,7 +4,7 @@ require_once ("Database.php");
 class ProductSearch
 {
     public function searchProduct($userId) {
-        $searchProduct = Database::query("SELECT product_id, title, about, sum, image FROM product WHERE user_id = '$userId'");
+        $searchProduct = Database::query("SELECT product_id, title, about, sum, image, phone FROM product WHERE user_id = '$userId'");
         if ($searchProduct) { // Успешный поиск списка продуктов
             if (mysqli_num_rows($searchProduct) > 1) { //Если мои товары найдены
                 $userData = mysqli_fetch_assoc($searchProduct); //Возвращаем список товаров в формате JSON
