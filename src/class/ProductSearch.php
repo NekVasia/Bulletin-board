99cdf03ab18671f4a0ac2c7ddeeec4a0aa7571de
nek_vasia <nek_vasia@mail.ru>
2023-12-17 11:43:34 +0300

    Подправил названия переменных

--- src/class/ProductSearch.php
+++ src/class/ProductSearch.php
@@ -7,8 +7,8 @@ class ProductSearch
         $searchProduct = Database::query("SELECT product_id, title, about, sum, image, phone FROM product WHERE user_id = '$userId'");
         if ($searchProduct) { // Успешный поиск списка продуктов
             if (mysqli_num_rows($searchProduct) > 1) { //Если мои товары найдены
-                $userData = mysqli_fetch_assoc($searchProduct); //Возвращаем список товаров в формате JSON
-                echo json_encode($userData); //Вывод полученных данных
+                $productData = mysqli_fetch_assoc($searchProduct); //Возвращаем список товаров в формате JSON
+                echo json_encode($productData); //Вывод полученных данных
             } else { //Если товар не найден
                 echo "Товар не найден";
             }
