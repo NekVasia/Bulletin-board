191871647c62c4af39b8a88f72a93987905868b0
nek_vasia <nek_vasia@mail.ru>
2023-12-18 22:24:04 +0300

    Переделал структуру userLogin

--- src/class/UserLogin.php
+++ src/class/UserLogin.php
@@ -6,22 +6,23 @@ class UserLogin
     public function userLogin($email, $password)
     {
         $userLogin = Database::query("SELECT user_id, password FROM users WHERE email = '$email'");
-        if ($userLogin) {
-            if (mysqli_num_rows($userLogin) == 1) {
-                $userData = mysqli_fetch_assoc($userLogin);
-                $dataBasePassword = $userData['password']; //Вытаскиваем password из базы данных
-                $id = $userData['user_id']; //Вытаскиваем id из базы данных
-                if ($password === $dataBasePassword) {
-                    $_SESSION['loggedIn'] = true; //Устанавливаем авторизацию в сессии
-                    $_SESSION['user_id'] = $id; //Устанавливаем id для сессии
-                } else { //Если пароль неверный
-                    echo "Неверный пароль";
-                }
-            } else { //Если пользователь не найден
-                echo "Пользователь не найден";
-            }
-        } else { //Ошибка при выполнении поиска
-            echo "Данный пользователь не зарегистрирован";
+        if (!$userLogin) {
+            echo "Данный пользователь не зарегистрирован!";
+            return;
+        }
+        if (!mysqli_num_rows($userLogin) == 1) {
+            echo "Пользователь не найден!";
+            return;
+        }
+        $userData = mysqli_fetch_assoc($userLogin);
+        $dataBasePassword = $userData['password']; //Вытаскиваем password из базы данных
+        $id = $userData['user_id']; //Вытаскиваем id из базы данных
+        if ($password === $dataBasePassword) {
+            $_SESSION['loggedIn'] = true; //Устанавливаем авторизацию в сессии
+            $_SESSION['user_id'] = $id; //Устанавливаем id для сессии
+            echo "Успешная авторизация";
+        } else { //Если пароль неверный
+            echo "Неверный пароль!";
         }
     }
 }
