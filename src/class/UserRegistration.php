7ead1ecb2e1bcd36357db552bd2d6926b391cc7f
nek_vasia <nek_vasia@mail.ru>
2023-12-18 22:24:45 +0300

    Добавил функцию проверки ранее зарегистрированных пользователей по вводимому email

--- src/class/UserRegistration.php
+++ src/class/UserRegistration.php
@@ -5,15 +5,20 @@ class UserRegistration
 {
     public function userRegistration($name, $email, $password, $phone)
     {
-        if (!empty($name) && !empty($email) && !empty($password) && !empty($phone)) {
-            $registration = Database::query("INSERT INTO users (name, email, password, phone) VALUES ('$name', '$email', '$password', '$phone')");
-            if ($registration) { // Успешная регистрация
-                echo "Пользователь успешно зарегистрирован";
-            } else { // Ошибка при регистрации
-                echo "Ошибка при регистрации пользователя";
-            }
-        } else {
-            echo "Некорректные данные для регистрации пользователя";
+        $checkRegistration = Database::query("SELECT email FROM users WHERE email = '$email'");
+        if (mysqli_num_rows($checkRegistration) == 1) {
+            echo "Данный пользователь зарегистрирован!";
+            return;
+        }
+        if (empty($name) && empty($email) && empty($password) && empty($phone)) {
+            echo "Некорректные данные для регистрации пользователя!";
+            return;
+        }
+        $registration = Database::query("INSERT INTO users (name, email, password, phone) VALUES ('$name', '$email', '$password', '$phone')");
+        if ($registration) { // Успешная регистрация
+            echo "Пользователь успешно зарегистрирован";
+        } else { // Ошибка при регистрации
+            echo "Ошибка при регистрации пользователя!";
         }
     }
 }
