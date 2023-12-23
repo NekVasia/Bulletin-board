4287c95c61f19202a6788d4982c4b9818e0b07be
nek_vasia <nek_vasia@mail.ru>
2023-12-23 12:35:47 +0300

    Реструктурирование и добавление своей базы

--- src/class/UserLogin.php
+++ src/class/UserLogin.php
@@ -6,27 +6,27 @@ class UserLogin
     public function userLogin($email, $password)
     {
         if (empty($email) || empty($password)) {
-            echo "Не заполнены данные пользователя!";
+            echo json_encode("Не заполнены данные пользователя!");
             return;
         }
         $userLogin = Database::query("SELECT user_id, password FROM users WHERE email = '$email'");
         if (!$userLogin) {
-            echo "Данный пользователь не зарегистрирован!";
+            echo json_encode("Данный пользователь не зарегистрирован!");
             return;
         }
         if (!mysqli_num_rows($userLogin) == 1) {
-            echo "Пользователь не найден!";
+            echo json_encode("Пользователь не найден!");
             return;
         }
-        $userData = mysqli_fetch_assoc($userLogin);
+        $userData = Database::fetch($userLogin);
         $dataBasePassword = $userData['password']; //Вытаскиваем password из базы данных
-        $id = $userData['user_id']; //Вытаскиваем id из базы данных
-        if ($password === $dataBasePassword) {
-            $_SESSION['loggedIn'] = true; //Устанавливаем авторизацию в сессии
-            $_SESSION['user_id'] = $id; //Устанавливаем id для сессии
-            echo "Успешная авторизация";
-        } else { //Если пароль неверный
-            echo "Неверный пароль!";
+        if ($password != $dataBasePassword) {
+            echo json_encode("Неверный пароль!");
+            return;
         }
+        $id = $userData['user_id']; //Вытаскиваем id из базы данных
+        $_SESSION['loggedIn'] = true; //Устанавливаем авторизацию в сессии
+        $_SESSION['user_id'] = $id; //Устанавливаем id для сессии
+        echo json_encode("Успешная авторизация");
     }
 }
