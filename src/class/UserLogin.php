110d9336767349b4054767951e79933430c328d8
nek_vasia <nek_vasia@mail.ru>
2023-12-28 20:48:46 +0300

    Добавил флажки для авторизации

--- src/class/UserLogin.php
+++ src/class/UserLogin.php
@@ -6,27 +6,27 @@ class UserLogin
     public function userLogin($email, $password)
     {
         if (empty($email) || empty($password)) {
-            echo json_encode("Не заполнены данные пользователя!");
+            echo json_encode(["code" => 0, "message" => "Не заполнены данные пользователя"]);
             return;
         }
         $userLogin = Database::query("SELECT user_id, password FROM users WHERE email = '$email'");
         if (!$userLogin) {
-            echo json_encode("Данный пользователь не зарегистрирован!");
+            echo json_encode(["code" => 0, "message" => "Данный пользователь не зарегистрирован!"]);
             return;
         }
         if (!mysqli_num_rows($userLogin) == 1) {
-            echo json_encode("Пользователь не найден!");
+            echo json_encode(["code" => 0, "message" => "Пользователь не найден!"]);
             return;
         }
         $userData = Database::fetch($userLogin);
         $dataBasePassword = $userData['password']; //Вытаскиваем password из базы данных
         if ($password != $dataBasePassword) {
-            echo json_encode("Неверный пароль!");
+            echo json_encode(["code" => 0, "message" => "Неверный пароль!"]);
             return;
         }
         $id = $userData['user_id']; //Вытаскиваем id из базы данных
         $_SESSION['loggedIn'] = true; //Устанавливаем авторизацию в сессии
         $_SESSION['user_id'] = $id; //Устанавливаем id для сессии
-        echo json_encode("Успешная авторизация");
+        echo json_encode(["code" => 1, "message" => "Успешная авторизация"]);
     }
 }
